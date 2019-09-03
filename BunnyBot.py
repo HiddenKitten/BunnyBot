2fddb76336a8cefca5266e17223379ffae336e91
TheHiddenGamer23 <30309397+TheHiddenGamer23@users.noreply.github.com>
2019-09-02 22:34:50 -0500

    command handling, easier writing help messages

--- BunnyBot.py
+++ BunnyBot.py
@@ -2,15 +2,50 @@ import asyncio
 import discord 
 from discord.ext import commands
 
+VERSION = '1.03'
+
+
+
+
+class Bot(commands.Bot):
+    async def send_cmd_help(self, ctx):
+        if ctx.invoked_subcommand:
+            pages = self.formatter.format_help_for(ctx, ctx.invoked_subcommand)
+            for page in pages:
+                await ctx.send(page)
+        else:
+            pages = await self.formatter.format_help_for(ctx, ctx.command)
+            for page in pages:
+                await ctx.send(page)
+
+
+bot = Bot("bb ", activity=discord.Game("Playing With My Food"))
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    """The event triggered when an error is raised while invoking a command.
+    ctx   : Context
+    error : Exception"""
+    if hasattr(ctx.command, 'on_error'):
+        return
+    elif isinstance(error, commands.NoPrivateMessage):
+        try:
+            return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
+        except:
+            pass
+    if isinstance(error, commands.MissingRequiredArgument):
+        await bot.send_cmd_help(ctx)
+
+
 async def is_owner(ctx):
     return ctx.author.id == 266079468135776258
 
 
-bot = commands.Bot("bb ", activity=discord.Game("Playing With My Food"))
 
 @bot.event
 async def on_ready():
-    print('Connected Successfully! Bunny Bot Version 1.01')
+    print('Connected Successfully! Bunny Bot Version %s' % VERSION)
 
 @bot.command(name="ping")
 async def _ping(ctx):
@@ -20,7 +55,7 @@ async def _ping(ctx):
 @bot.command(name="version")
 async def _version(ctx):
     """shows the version info"""
-    await ctx.send("Version 1.01")
+    await ctx.send("Version %s" % VERSION)
 
 @bot.command(name="changelog")
 async def _changelog(ctx):
@@ -44,9 +79,6 @@ async def _poll(ctx, text, *options):
     for i in reactions:
         await msg.add_reaction(i)
 
-
-
-
 @commands.check(is_owner)
 @bot.command(name="debug", hidden=True)
 async def _debug(ctx, *, code):
@@ -59,8 +91,7 @@ async def _stop(ctx):
     await ctx.send("Shutting Down.")
     await bot.logout()
 
-
-
-with open('.token', 'r') as f:
-    __token = f.readline()
-bot.run(__token)
+if __name__ == '__main__':
+    with open('.token', 'r') as f:
+        __token = f.readline()
+    bot.run(__token)
