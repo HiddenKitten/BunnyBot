d4e0498c4cbdaafed6cddb851eca9cb80293c8c2
TheHiddenGamer23 <30309397+TheHiddenGamer23@users.noreply.github.com>
2020-04-29 21:54:48 -0500

    enabled server info

--- BunnyBot.py
+++ BunnyBot.py
@@ -302,23 +302,28 @@ async def _adminlinks(ctx):
     with open('data/admin.txt') as f:
         await ctx.author.send(f.read())
 
-# @bot.command(name='gameservers', aliases=['servers'])
-# async def _gameservers(ctx):
-#     from utils import tf2, q3
-#     tf2 = await tf2.tf2ping()
-#     quake = await q3.q3ping()
-#     e = discord.Embed(title='Team Fortress 2', description=tf2['name'])
-#     for k, v in tf2.items():
-#         if k != 'name':
-#             e.add_field(name=k, value=v)
-#     await ctx.send(embed=e)
-#     s = ['name-------- frags ping'] 
-#     for i in quake[1]:
-#         s.append('{0:-<12} {1:-<5} {2:-<4}'.format(i['name'].strip('"'), i['frags'], i['ping']))
-#     e = discord.Embed(title='Quake 3 Arena', description='\n'.join(s))
-#     for k, v in quake[0].items():
-#         e.add_field(name=k, value=v)
-#     await ctx.send(embed=e)
+@bot.command(name='gameservers', aliases=['servers'])
+async def _gameservers(ctx):
+    from utils import tf2, q3
+    tf2 = await tf2.tf2ping()
+    quake = await q3.q3ping()
+    e = discord.Embed(title='Team Fortress 2', description=tf2['name'])
+    for k, v in tf2.items():
+        if k in ['map', 'players', 'maxplayers', 'secured', 'version']:
+            e.add_field(name=k, value=v)
+    e.add_field(name='IP', value='meemteam.co')
+    e.color = discord.Colour.green()
+    await ctx.send(embed=e)
+    s = ['name-------- frags ping'] 
+    for i in quake[1]:
+        s.append('{0:-<12} {1:-<5} {2:-<4}'.format(i['name'].strip('"'), i['frags'], i['ping']))
+    e = discord.Embed(title=quake[0]['sv_hostname'], description='\n'.join(s))
+    for k, v in quake[0].items():
+        if k in ['mapname', 'timelimit', 'fraglimit']:
+            e.add_field(name=k, value=v)
+    e.add_field(name='IP', value='meemteam.co')
+    e.color = discord.Colour.green()
+    await ctx.send(embed=e)
 
 # Internals
 
